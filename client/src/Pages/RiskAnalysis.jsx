03059c60c13c377fcaf8f13fbcbbd12027d131a9
AditiDubey1 <aditidubey.ad.6@gmail.com>
2023-10-20 19:10:55 +0530

    Adding changes to Risk Analysis page.

--- client/src/Pages/RiskAnalysis.jsx
+++ client/src/Pages/RiskAnalysis.jsx
@@ -6,9 +6,11 @@ import Button from "@mui/material/Button";
 import Stack from '@mui/material/Stack';
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-
-
-
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 export function RiskAnalysis() {
 
@@ -35,10 +37,10 @@ export function RiskAnalysis() {
 >
   <Grid container spacing={4}>
     <Grid item xs={12} sm={6} >
-    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}} > Is there any risk associated with : (ü/û)  </Typography>
+    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}} > Is there any risk associated with : (ü/û) * </Typography>
       <TextField
         required
-        id="Is there any risk associated with : (ü/û) "
+        id="Is there any risk associated with : (ü/û) *"
         label="Enter Details"
         variant="outlined"
         fullWidth
@@ -46,11 +48,11 @@ export function RiskAnalysis() {
       />
     </Grid>
     <Grid item xs={12} sm={6}>
-    <Typography variant="subtitle1"  sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}} > Manufacturing Requirement  </Typography>
+    <Typography variant="subtitle1"  sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}} > Manufacturing Requirement * </Typography>
 
       <TextField
         required
-        id=" Manufacturing Requirement "
+        id=" Manufacturing Requirement *"
         label="Enter Details  "
         variant="outlined"
         fullWidth
@@ -58,11 +60,11 @@ export function RiskAnalysis() {
       />
     </Grid>
     <Grid item xs={12} sm={6}>
-    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}}  > Application </Typography>
+    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}}  > Application *</Typography>
 
       <TextField
         required
-        id="Application "
+        id="Application *"
         label="Enter Details"
         variant="outlined"
         fullWidth
@@ -71,29 +73,155 @@ export function RiskAnalysis() {
     </Grid>
     <Grid item xs={12} sm={6}>
     <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.2rem', padding:'0.3rem'}}  >Internal Estimation </Typography>
-
-      <TextField
+     <TextField
         required
-        id="Internal Estimation "
+        id="Internal Estimation *"
         label="Enter Details"
         variant="outlined"
         fullWidth
         size="small"
       />
     </Grid>
-    <Grid item xs={12} sm={12}>
+
+    
+
+    <Grid item xs={12} sm={6}>
+    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.25rem', padding:'0.3rem'}}>Risk</Typography>
+     <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.15rem', padding:'0.3rem'}}>Enviorment:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    
+    <TextField
+      label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.1rem', padding:'0.3rem'}}>Investment:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+       <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    <TextField
+        
+        label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+
+<FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.1rem', padding:'0.3rem'}}>Manufacturing:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+       <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    <TextField
+        
+        label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+</Grid>
+    
+<Grid item xs={12} sm={6}>
+    <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470',fontWeight: '650', fontSize: '1.25rem', padding:'0.3rem'}}>Guidelines</Typography>
+     <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.15rem', padding:'0.3rem'}}>Technical Feasibiltiy:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    
+    <TextField
+        
+        label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.1rem', padding:'0.3rem'}}>Estimation:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+       <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    <TextField
+        
+        label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+
+<FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label" sx={{ textAlign: 'left', color: '#054470',fontWeight: '450', fontSize: '1.1rem', padding:'0.3rem'}}>Regrade:</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+       <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="NO" control={<Radio />} label="No" />
+       </RadioGroup>
+    </FormControl>
+    <TextField
+        
+        label="Add Remarks"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+</Grid>
+
+  
+  
+
+      
+ 
+  
+ <Grid item xs={12} sm={12}>
   <Typography variant="subtitle1" sx={{ textAlign: 'left', color: '#054470', fontWeight: '650', fontSize: '1.2rem' }}> Remarks </Typography>
   <TextField
+  required
+  id="Remark"
   label="Enter Remarks"
-  id="Remarks"
- variant="outlined"
+  variant="outlined"
   fullWidth
   size="small"
   />
+     </Grid> 
 </Grid>
-    
-    
-  </Grid>
   <Stack direction="row" spacing={5} justifyContent="center"  mt={4} sx={{ mb: 4 }}>
   <Link to={"/rfq"}>
   <Button variant="contained"  size="large">
