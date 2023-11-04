2017766ac5a73b459f0b99259bf98a9a0123682d
sid120 <siddharthrakshe12@gmail.com>
2023-11-04 22:56:37 +0530

    updated the whole code for user approval page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,169 @@
-import './App.css'
-import Dashboard from './Pages/Dashboard';
-import { Login } from './Pages/Login';
-import { Register } from './Pages/register';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Process from './Pages/Process';
-import Approval from './Pages/Approval';
-import ChangePassword from './Pages/ChangePassword';
-import Summary from './Pages/Summary';
-import { UserApproval } from './Pages/UserApproval';
-function App() {
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import NavBar from './Components/NavBar';
+
+const defaultTheme = createTheme();
+
+const UserApproval = () => {
+  const emailRegex = /^\w+([\.-]?\w+)*@aakarfoundry\.com$/;
+  const phoneRegex = /^\d{10}$/;
+
+  const [emailError, setEmailError] = React.useState(false);
+  const [phoneError, setPhoneError] = React.useState(false);
+  const [department, setDepartment] = React.useState('');
+  const [role, setRole] = React.useState('');
+
+  const handleDepartmentChange = (event) => {
+    setDepartment(event.target.value);
+  };
+
+  const handleRoleChange = (event) => {
+    setRole(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const phoneNumber = data.get('phoneNumber');
+
+    setEmailError(!emailRegex.test(email));
+    setPhoneError(!phoneRegex.test(phoneNumber));
+
+    if (!emailRegex.test(email) || !phoneRegex.test(phoneNumber) || !department || !role) {
+      return;
+    }
+
+    // Proceed with form submission
+    console.log({
+      email: email,
+      fullName: data.get('name'),
+      phoneNumber: phoneNumber,
+      department: department,
+      role: role,
+    });
+  };
+
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+      <NavBar/>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 15,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Create User
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} Validate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Full Name
+                </Typography>
+                <TextField
+                  required
+                  id="name"
+                  label="Full Name"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Email Address
+                </Typography>
+                <TextField
+                  required
+                  error={emailError}
+                  helperText={emailError ? 'Invalid email address' : ''}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Phone Number
+                </Typography>
+                <TextField
+                  required
+                  error={phoneError}
+                  helperText={phoneError ? 'Invalid phone number' : ''}
+                  id="phoneNumber"
+                  label="Phone Number"
+                  name="phoneNumber"
+                  autoComplete="tel"
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Department
+                </Typography>
+                <Select
+                  required
+                  id="department"
+                  value={department}
+                  onChange={handleDepartmentChange}
+                  label="Department"
+                  style={{ width: '100%' }}
+                >
+                  <MenuItem value="Marketing">Marketing</MenuItem>
+                  <MenuItem value="Machine">Machine</MenuItem>
+                  <MenuItem value="Quality">Quality</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Role/Title
+                </Typography>
+                <Select
+                  required
+                  id="role"
+                  value={role}
+                  onChange={handleRoleChange}
+                  label="Role/title"
+                  style={{ width: '100%' }}
+                >
+                  <MenuItem value="CEO">CEO</MenuItem>
+                  <MenuItem value="HOD">HOD</MenuItem>
+                  <MenuItem value="Employee">Employee</MenuItem>
+                </Select>
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Add User
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dash" element={<Dashboard />} />
-          <Route path="/details/:id" element={<Process />} />
-          <Route path='/details' element={<Process />} />
-          {/* <Route path="/customer" element={<CustomerName />} />
-          <Route path="/rfq" element={<RfqAttributes />} />
-          <Route path="/risk" element={<RiskAnalysis />} />
-          <Route path="/design" element={<DesignFoundry />} />
-          <Route path="/machine" element={<MachineQuality />} />
-          <Route path="/prod" element={<NewProductDev />} /> */}
-          <Route path="/approval" element={<Approval />} />
-          <Route path="/summary" element={<Summary />} />
-          <Route path="/user" element={<UserApproval />} />
-
-          <Route path="/change-password" element={<ChangePassword />} />
-        </Routes>
-      </Router>
-    </>
-  )
-}
-
-export default App
+export default UserApproval;
--- src/Pages/UserApproval.jsx
+++ src/Pages/UserApproval.jsx
@@ -1,351 +1,146 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import Alert from "@mui/material/Alert";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import { useEffect, useState } from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const defaultTheme = createTheme();
 
+export default function UserApproval() {
+  const emailRegex = /^\w+([\.-]?\w+)*@aakarfoundry\.com$/;
+  const phoneRegex = /^\d{10}$/;
 
-export const UserApproval = () => {
-
-
-    const roleOptions = ["CEO", "HOD", "Employee"];
-  const departmentOptions = ["IT", "Marketing", "Machining"];
-
-
-    const [selectedRole, setSelectedRole] = useState("");
-  const [selectedDepartment, setSelectedDepartment] = useState("");
-
-  const handleEvent = (e) => {
-    setForm({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const [formData, setForm] = useState({});
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [isFailure, setIsFailure] = useState(false);
-  const [isNameValid, setIsNameValid] = useState(true);
-  const [isEmailValid, setIsEmailValid] = useState(true);
-  const [isRoleValid, setIsRoleValid] = useState(true);
-  const [isNumberValid, setIsNumberValid] = useState(true);
-  const [isDepartmentValid, setIsDepartmentValid] = useState(true);
-
-  function isfullName(val) {
-    if (val) {
-        var regex = /^[a-zA-Z.-\s]*$/;
-        const isValid = regex.test(val) && val.trim().length > 0;
-        setIsNameValid(isValid);
-        return isValid;
-      } else {
-        // Handle the case when val is undefined or empty
-        setIsNameValid(false);
-        return false;
-      }
-  }
-
-  function isMail(val) {
-    var mail = /^\w+([\.-]?\w+)*@aakarfoundry\.com$/;
-    const isValid = mail.test(val);
-    setIsEmailValid(isValid); 
-    return isValid;
-  }
-
-  function isNumber(val) {
-    var phoneNo = /^\d{10}$/;
-    const isValid = phoneNo.test(val);
-    setIsNumberValid(isValid); 
-    return isValid;
-  }
-  
-  function isRole(val) {
-    if (val) {
-        var regex = /^[a-zA-Z.-\s]*$/;
-        const isValid = regex.test(val) && val.trim().length > 0;
-        setIsRoleValid(isValid);
-        return isValid;
-      } else {
-        // Handle the case when val is undefined or empty
-        setIsRoleValid(false);
-        return false;
-      }
-  }
-  
-
-  function isDepartmentBox(val) {
-    if (val) {
-        var regex = /^[a-zA-Z.-\s]*$/;
-        const isValid = regex.test(val) && val.trim().length > 0;
-        setIsDepartmentValid(isValid);
-        return isValid;
-      } else {
-        // Handle the case when val is undefined or empty
-        setIsDepartmentValid(false);
-        return false;
-      }
-  }
-
-  useEffect(() => {
-    const closeAlerts = () => {
-      setIsSuccess(false);
-      setIsFailure(false);
-    };
-
-    const timer = setTimeout(closeAlerts, 2000);
-
-    return () => clearTimeout(timer);
-  }, [isSuccess, isFailure]);
-  function onFormSubmit(e) {
-    e.preventDefault();
-    var name = isfullName(formData.name);
-    var email = isMail(formData.email);
-    var number = isNumber(formData.number);
-    var department = isDepartmentBox(formData.department);
-    
-    var number = isRole(formData.role);
-
-    if (name && email && department && number) {
-      console.log(formData);
-      setIsSuccess(true);
-      setIsFailure(false);
-    } else {
-      setIsSuccess(false);
-      setIsFailure(true);
-    }
-  }
-
-  useEffect(() => {
-    setForm({
-      name: "",
-      email: "",
-      number: "",
-      
-    });
-    setSelectedRole(""); // Reset selected role
-    setSelectedDepartment("");
-  }, []);
+  const [emailError, setEmailError] = React.useState(false);
+  const [phoneError, setPhoneError] = React.useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const phoneNumber = data.get('phoneNumber');
+
+    setEmailError(!emailRegex.test(email));
+    setPhoneError(!phoneRegex.test(phoneNumber));
+
+    if (!emailRegex.test(email) || !phoneRegex.test(phoneNumber)) {
+      return;
+    }
+
+    // Proceed with form submission
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      email: email,
+      password: data.get('password'),
+      fullName: data.get('name'),
+      phoneNumber: phoneNumber,
+      role: data.get('role'),
+      department: data.get('department'),
     });
   };
 
   return (
-  
-       
-        <Container
-          component="main"
-          maxWidth="xs"
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center", 
-            justifyContent: "center", 
-            height: "100vh",
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Box
-            sx={{
-              marginTop: 15,
-              marginLeft: 0,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5" sx={{ color: "black" }}>
-              Create User
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
+          <Typography component="h1" variant="h5">
+            Create User
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} Validate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Full Name
+                </Typography>
+                <TextField
+                  required
+                  id="name"
+                  label="Full Name"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Email Address
+                </Typography>
+                <TextField
+                  required
+                  error={emailError}
+                  helperText={emailError ? 'Invalid email address' : ''}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Phone Number
+                </Typography>
+                <TextField
+                  required
+                  error={phoneError}
+                  helperText={phoneError ? 'Invalid phone number' : ''}
+                  id="phoneNumber"
+                  label="Phone Number"
+                  name="phoneNumber"
+                  autoComplete="tel"
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Role/Title
+                </Typography>
+                <TextField
+                  required
+                  id="role"
+                  label="Role/title"
+                  name="role"
+                  autoComplete="role"
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1" style={{ textAlign: 'left' }}>
+                  Department
+                </Typography>
+                <TextField
+                  required
+                  id="department"
+                  label="Department"
+                  name="department"
+                  autoComplete="organization"
+                  style={{ width: '100%' }}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                sx={{
-                  "& .MuiInputLabel-root": { color: "black" }, 
-                  "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { border: 0.5, borderColor: "black" },
-                  },
-                }}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Full Name"
-                name="name"
-                autoComplete="name"
-                onChange={handleEvent}
-                autoFocus
-                helperText={isNameValid ? "" : "Invalid name"}
-                error={!isNameValid}
-              />
-              <TextField
-                sx={{
-                  "& .MuiInputLabel-root": { color: "black" }, 
-                  "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { border: 0.5, borderColor: "black" },
-                  },
-                }}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                onChange={handleEvent}
-                autoFocus
-                helperText={isEmailValid ? "" : "Invalid Email"}
-                error={!isEmailValid}
-              />
-              <TextField
-                sx={{
-                  "& .MuiInputLabel-root": { color: "black" }, 
-                  "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { border: 0.5, borderColor: "black" },
-                  },
-                }}
-                margin="normal"
-                required
-                fullWidth
-                name="number"
-                label="Phone Number"
-                type="text"
-                id="number"
-                onChange={handleEvent}
-                autoComplete=""
-                helperText={isNumberValid ? "" : "Invalid phone number."}
-                error={!isNumberValid}
-              />
-              <FormControl
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                required
-                sx={{
-                "& .MuiInputLabel-root": { color: isRoleValid ? "black" : "red" },
-                "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { border: 0.5, borderColor: isRoleValid ? "black" : "red" },
-                },
-                }}
-            >
-                <InputLabel htmlFor="role">Role</InputLabel>
-                <Select
-                value={selectedRole}
-                onChange={(e) => setSelectedRole(e.target.value)}
-                label="Role"
-                >
-                {roleOptions.map((option) => (
-                    <MenuItem key={option} value={option}>
-                    {option}
-                    </MenuItem>
-                ))}
-                </Select>
-          </FormControl>
-          <FormControl
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            required
-            sx={{
-              "& .MuiInputLabel-root": { color: isRoleValid ? "black" : "red" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { border: 0.5, borderColor: isDepartmentValid ? "black" : "red" },
-              },
-            }}
-          >
-            <InputLabel htmlFor="department">Department</InputLabel>
-            <Select
-              value={selectedDepartment}
-              onChange={(e) => setSelectedDepartment(e.target.value)}
-              label="Department"
-            >
-              {departmentOptions.map((option) => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: "#1565C0" }}
-                onClick={onFormSubmit}
-              >
-                Add USer
-              </Button>
-              {/*<div className={styles.registerLine}>
-                
-                Already a Member?&nbsp;
-                <Link href="/" variant="body2">
-                  {"Login"}
-                </Link>
-            </div>*/}
-            </Box>
+              Add User
+            </Button>
           </Box>
-
-          {isSuccess && (
-            <Alert
-              severity="success"
-              color="info"
-              style={{
-                backgroundColor: "#4CAF50",
-                color: "white",
-                borderRadius: "0.25rem",
-                width: "18%",
-                position: "fixed",
-                top: "2rem",
-                left: "50%",
-                transform: "translateX(-50%)",
-                zIndex: 1000,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "2rem",
-              }}
-              onClose={() => setIsSuccess(false)}
-            >
-              Registration Successful!
-            </Alert>
-          )}
-
-          {isFailure && (
-            <Alert
-              severity="error"
-              color="info"
-              style={{
-                backgroundColor: "red",
-                color: "white",
-                borderRadius: "0.25rem",
-                width: "18%",
-                position: "fixed",
-                top: "2rem",
-                left: "50%",
-                transform: "translateX(-50%)",
-                zIndex: 1000,
-                display: "flex",
-                alignItems: "center",    
-                justifyContent: "center",
-                height: "2rem",
-              }}
-              onClose={() => setIsFailure(false)}
-            >
-              Registration Failed.
-            </Alert>
-          )}
-        </Container>
-     
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
-};
+}
